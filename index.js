b2f22cf253a034c591bfbd11647362cbaf58a0f3
Temba Sydney Gabriel <tembasydney@gmail.com>
2024-04-17 13:54:40 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+
 function addingEventListener() {
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
 }
+
+// Invoke the addingEventListener function when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    addingEventListener();
+});
